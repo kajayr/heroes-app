7d961e8a7a4b158a5eaedc90868b9d393081dfad
Kajayr <kajayr_sinanian@yahoo.com>
2022-03-06 21:57:36 -0600

    added messages to heroes for fetching the data while clicking on it

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import { HeroService } from '../hero.service';
+import {MessageService} from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -13,7 +14,7 @@ name: "Windstorm"
 }
 heroes:Hero[] = [];
 selectedHero?:Hero;
-  constructor(private heroService:HeroService) { }
+  constructor(private heroService:HeroService, private messageService:MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -21,6 +22,7 @@ selectedHero?:Hero;
 
   onSelect(data:Hero){
     this.selectedHero = data;
+    this.messageService.add("Hero " + data.name + " was fetched");
   }
   getHeroes(){
   this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
