867a283a046fc38c0bd6e05d8456cdfea9a7e76e
Kajayr <kajayr_sinanian@yahoo.com>
2022-03-08 01:29:00 -0600

    added router to components and heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,29 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import { HeroService } from '../hero.service';
-import {MessageService} from '../message.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-hero:Hero = {
-id: 1,
-name: "Windstorm"
-}
 heroes:Hero[] = [];
-selectedHero?:Hero;
-  constructor(private heroService:HeroService, private messageService:MessageService) { }
+  constructor(private heroService:HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(data:Hero){
-    this.selectedHero = data;
-    this.messageService.add("Hero " + data.name + " was fetched");
-  }
   getHeroes(){
   this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
