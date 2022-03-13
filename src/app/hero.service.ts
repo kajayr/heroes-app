5fb99a293797be5a3ffb22df361bc5b2ea2a3a87
Kajayr <kajayr_sinanian@yahoo.com>
2022-03-12 20:11:33 -0600

    added http service to the angular. getting data fom api

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,19 +3,24 @@ import { Observable, of } from 'rxjs';
 import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 import {MessageService} from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService:MessageService) { }
+  constructor(private messageService:MessageService, private http:HttpClient) { }
 
   getHeroes(): Observable<Hero[]>{
-    const heroes = of(HEROES);
-    this.messageService.add("Hero Service: Fetched heroes");
-    return heroes;
+    // const heroes = of(HEROES);
+    // this.messageService.add("Hero Service: Fetched heroes");
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+  private heroesUrl = 'api/heroes';  // URL to web api
   getHero(id: number): Observable<Hero> {
     // For now, assume that a hero with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
