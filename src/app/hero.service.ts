bc696954ee58200538c595092c6ab3d6667669fc
Kajayr <kajayr_sinanian@yahoo.com>
2022-03-13 23:53:35 -0500

    updating data using http put() method through observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -55,4 +55,16 @@ getHeroes(): Observable<Hero[]>{
     catchError(this.handleError<Hero[]>('getHeroes', []))
   );
 }
+
+httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+/** PUT: update the hero on the server */
+updateHero(hero: Hero): Observable<any> {
+  return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+    tap(_ => this.log(`updated hero id=${hero.id}`)),
+    catchError(this.handleError<any>('updateHero'))
+  );
+}
 }
